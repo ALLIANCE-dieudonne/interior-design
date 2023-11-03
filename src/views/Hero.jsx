f6223986053edf1e4d2c98647a66f3cc92f01988
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-11-04 01:39:51 +0200

    done with faded in

--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -7,7 +7,7 @@ import logo from "../assets/logo.svg";
 import cartIcon from "../assets/cart-icon.svg";
 import searchIcon from "../assets/search-icon.svg";
 
-import { heroTitle, heroSubtitle } from "../data";
+import { heroTitle, heroSubtitle, services } from "../data";
 
 import FadeIn from "../components/FadeIn";
 import NavLink from "../components/NavLink";
@@ -17,19 +17,71 @@ import { HiMenuAlt3 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Hero = () => {
-  return <div className = "h-screen relative flex flex-col items-center" style={{ 
-    background: `url(${heroImage})`,
-    backgroundPosition: "bottom",
-    backgroundRepeat : "no-repeat",
-    backgroundSize: "cover"
-   }}>
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  return (
+    <div
+      className="h-screen relative flex flex-col items-center"
+      style={{
+        background: `url(${heroImage})`,
+        backgroundPosition: "bottom",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="relative w-full max-w-[1490px] flex items-center justify-between p-10 mx-auto">
+        <img src={logo} alt="" />
 
-    <div className="relative w-full max-w-[1490px] flex items-center pt-10 justify-between mx-auto px-10">
-      <img src={logo} alt="" />
+        <ul className="hidden md:flex items-center gap-10 lg:gap-[68px]">
+          <NavLink to="services">Services</NavLink>
+          <NavLink to="products">Shop</NavLink>
+          <NavLink to="reference">Reference</NavLink>
+          <NavLink to="care">Care</NavLink>
+        </ul>
 
-      <ul className="hidden md:flex"></ul>
+        <img src={cartIcon} alt="" className="hidden md:block cursor-pointer" />
+        <HiMenuAlt3
+          size={30}
+          className="block md:hidden cursor-pointer text-white"
+          onClick={() => setShowMobileMenu((prev) => !prev)}
+        />
+
+        <div
+          className={`block md:hidden w-full fixed ${
+            !showMobileMenu ? "-top-[410px] " : "top-0"
+          } left-0 bg-[#dde0e5] h-[410px] transition-all duration-[800ms] shadow-xl z-10 py-8 px-12 rounded-b-xl`}
+        >
+          <AiOutlineClose
+            size={25}
+            className="absolute top-5 right-5 cursor-pointer"
+            onClick={() => setShowMobileMenu(false)}
+          />
+
+          <ul className="pt-[60px] items-center flex flex-col gap-8">
+            <NavLink to="services" mobileMenu>
+              Services
+            </NavLink>
+            <NavLink to="products" mobileMenu>
+              Shop
+            </NavLink>
+            <NavLink to="reference" mobileMenu>
+              Reference
+            </NavLink>
+            <NavLink to="care" mobileMenu>
+              Care
+            </NavLink>{" "}
+          </ul>
+
+          <img src={cartIcon} alt="" className=" mt-8 mx-auto cursor-pointer" />
+        </div>
+      </div>
+
+      <FadeIn delay={0.2} direction="down" padding fullWidth>
+        <h1 className="mt-[90px] text-center text-5xl leading-tight xs:text-[64px] text-white max-w-[1050px]">
+          {heroTitle}
+        </h1>
+      </FadeIn>
     </div>
-   </div>;
+  );
 };
 
 export default Hero;
