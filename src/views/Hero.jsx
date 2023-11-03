e6d70c4efb7f6ef419cbdd3d7795eb1bef79a972
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-11-04 00:22:23 +0200

    initial  commit

---
+++ src/views/Hero.jsx
@@ -0,0 +1,35 @@
+// React
+import { useState } from "react";
+
+// local imports
+import heroImage from "../assets/hero-image.svg";
+import logo from "../assets/logo.svg";
+import cartIcon from "../assets/cart-icon.svg";
+import searchIcon from "../assets/search-icon.svg";
+
+import { heroTitle, heroSubtitle } from "../data";
+
+import FadeIn from "../components/FadeIn";
+import NavLink from "../components/NavLink";
+
+// react icons
+import { HiMenuAlt3 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+const Hero = () => {
+  return <div className = "h-screen relative flex flex-col items-center" style={{ 
+    background: `url(${heroImage})`,
+    backgroundPosition: "bottom",
+    backgroundRepeat : "no-repeat",
+    backgroundSize: "cover"
+   }}>
+
+    <div className="relative w-full max-w-[1490px] flex items-center pt-10 justify-between mx-auto px-10">
+      <img src={logo} alt="" />
+
+      <ul className="hidden md:flex"></ul>
+    </div>
+   </div>;
+};
+
+export default Hero;
